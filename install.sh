52e502555a077575159f807e5fc3b61642a6c300
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-05-05 21:17:23 -0300

    added mid_install

--- install.sh
+++ install.sh
@@ -51,6 +51,62 @@ mount_disk(){
 	lsblk -f
 }
 
+time_and_locale(){
+  # Links to your timezone
+  ln -sf /mnt/usr/share/zoneinfo/America/Sao_Paulo /mnt/etc/localtime
+  
+  # generate /etc/adjtime
+  arch-chroot /mnt hwclock --systohc
+  
+  # Set locale to br
+  echo 'pt_BR.UTF-8 UTF-8' >> /mnt/etc/locale.gen
+  arch-chroot /mnt locale-gen
+  echo 'LANG=pt_BR.UTF-8' >> /mnt/etc/locale.conf
+}
+
+packages(){
+  # Pacman config
+  sed -i 's/#Color/Color/' /mnt/etc/pacman.conf
+  sed -i 's/#ParallelDownloads/ParallelDownloads/' /mnt/etc/pacman.conf
+  sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
+  
+  # Install all needed packages
+  arch-chroot /mnt pacman -Sy --noconfirm --needed - < packages.txt
+}
+
+grub(){
+	# Set timeout = 0
+	sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /mnt/etc/default/grub
+	# Enable os-prober
+	sed -i 's/\#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/' /mnt/etc/default/grub
+	
+	# Mount boot/EFI partition
+	arch-chroot /mnt mount --mkdir /dev/sda1 /boot/EFI
+	
+	# Print partition table
+	arch-chroot /mnt lsblk -f
+	
+	# Install grub with uefi
+	arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+systems(){
+	# Enable internet, VM, Printing, Bluetooth
+	for system in NetworkManager libvirtd cups bluetooth;
+	do 
+		arch-chroot /mnt systemctl enable $system
+	done
+}
+
+create_user(){
+	for user in work fun;
+	do 
+		arch-chroot /mnt useradd -m -G wheel,audio,video,optical,storage,libvirt -s /bin/fish $user
+		arch-chroot /mnt passwd echo "$user:1234" | chpasswd
+	done
+}
+
 is_uefi
 partition
 format
@@ -63,3 +119,24 @@ timedatectl set-ntp true
 pacstrap -K /mnt base linux linux-firmware
 # Permanent mount partitions
 genfstab -U /mnt >> /mnt/etc/fstab 
+time_and_locale
+# Set console keyboard to br
+echo 'KEYMAP=br-abnt2' >> /mnt/etc/vconsole.conf
+# Host name
+echo 'arch' >> /mnt/etc/hostname
+packages
+# Only the group wheel has superuser permission
+echo 'permit keepenv persist :wheel' >> /mnt/etc/doas.conf
+# Blacklists nouveau in case nvidia-utils doesn't
+echo 'blacklist nouveau' >> /mnt/etc/modprobe.d/blacklist.conf
+# Change shell to fish
+arch-chroot /mnt chsh -s /bin/fish
+grub
+systems
+# Root password
+arch-chroot /mnt echo "root:1234" | chpasswd
+create_user
+# Ensure blacklist works
+arch-chroot /mnt mkinitcpio -P
+# Save any logs in home
+cp -v *.log /mnt/home/work/
