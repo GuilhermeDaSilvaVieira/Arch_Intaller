cbae38a24020060a3ef78e08f7d6bdced3f04be7
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-05-08 14:28:38 -0300

    test: run chroot as user

--- install.sh
+++ install.sh
@@ -9,140 +9,138 @@ RED='\033[0;31m'
 NO_COLOR='\033[0m'
 
 is_uefi() {
-	DIRECTORY_UEFI="/sys/firmware/efi/efivars/"
-	if [ -d $DIRECTORY_UEFI ]; then
-		echo -e "${GREEN}Supports UEFI${NO_COLOR}"
-		echo ""
-	else
-		echo -e "${RED}Doesn't Support UEFI${NO_COLOR}"
-		echo -e "${RED}This is a UEFI only script${NO_COLOR}"
-		exit 0
-	fi
+    DIRECTORY_UEFI="/sys/firmware/efi/efivars/"
+    if [ -d $DIRECTORY_UEFI ]; then
+        echo -e "${GREEN}Supports UEFI${NO_COLOR}"
+        echo ""
+    else
+        echo -e "${RED}Doesn't Support UEFI${NO_COLOR}"
+        echo -e "${RED}This is a UEFI only script${NO_COLOR}"
+        exit 0
+    fi
 }
 
 partition(){
-	# /boot/EFI [SWAP] /
-	fdisk /dev/sda < fdisk_cmds  
-	# /home
-	fdisk /dev/nvme0n1 < home_fdisk_cmds 
+    # /boot/EFI [SWAP] /
+    fdisk /dev/sda < fdisk_cmds
+    # /home
+    fdisk /dev/nvme0n1 < home_fdisk_cmds
 }
 
 format(){
-	# System
-	mkfs.ext4 /dev/sda3
-	# Boot
-	mkfs.fat -F32 /dev/sda1
-	# Swap
-	mkswap /dev/sda2
-	# Home
-	mkfs.ext4 /dev/nvme0n1p1
+    # System
+    mkfs.ext4 /dev/sda3
+    # Boot
+    mkfs.fat -F32 /dev/sda1
+    # Swap
+    mkswap /dev/sda2
+    # Home
+    mkfs.ext4 /dev/nvme0n1p1
 }
 
 mount_disk(){
-	# System
-	mount /dev/sda3 /mnt
-	# Boot
-	mount --mkdir /dev/sda1 /mnt/boot/EFI
-	# Swap
-	swapon /dev/sda2
-	# Home
-	mount --mkdir /dev/nvme0n1p1 /mnt/home
-	# Prints partition table
-	lsblk -f
+    # System
+    mount /dev/sda3 /mnt
+    # Boot
+    mount --mkdir /dev/sda1 /mnt/boot/EFI
+    # Swap
+    swapon /dev/sda2
+    # Home
+    mount --mkdir /dev/nvme0n1p1 /mnt/home
+    # Prints partition table
+    lsblk -f
 }
 
 time_and_locale(){
-	# Links to your timezone
-	ln -sf /mnt/usr/share/zoneinfo/America/Sao_Paulo /mnt/etc/localtime
+    # Links to your timezone
+    ln -sf /mnt/usr/share/zoneinfo/America/Sao_Paulo /mnt/etc/localtime
 
-  # generate /etc/adjtime
-  arch-chroot /mnt hwclock --systohc
+    # generate /etc/adjtime
+    arch-chroot /mnt hwclock --systohc
 
-  # Set locale to br
-  echo 'pt_BR.UTF-8 UTF-8' >> /mnt/etc/locale.gen
-  arch-chroot /mnt locale-gen
-  echo 'LANG=pt_BR.UTF-8' >> /mnt/etc/locale.conf
+    # Set locale to br
+    echo 'pt_BR.UTF-8 UTF-8' >> /mnt/etc/locale.gen
+    arch-chroot /mnt locale-gen
+    echo 'LANG=pt_BR.UTF-8' >> /mnt/etc/locale.conf
 }
 
 packages(){
-	# Pacman config
-	sed -i 's/#Color/Color/' /mnt/etc/pacman.conf
-	sed -i 's/#ParallelDownloads/ParallelDownloads/' /mnt/etc/pacman.conf
-	sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
+    # Pacman config
+    sed -i 's/#Color/Color/' /mnt/etc/pacman.conf
+    sed -i 's/#ParallelDownloads/ParallelDownloads/' /mnt/etc/pacman.conf
+    sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
 
-  # Install all needed packages
-  arch-chroot /mnt pacman -Sy --noconfirm --needed - < packages.txt
+    # Install all needed packages
+    arch-chroot /mnt pacman -Sy --noconfirm --needed - < packages.txt
 }
 
 grub(){
-	# Set timeout = 0
-	sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /mnt/etc/default/grub
-	# Enable os-prober
-	sed -i 's/\#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/' /mnt/etc/default/grub
+    # Set timeout = 0
+    sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /mnt/etc/default/grub
+    # Enable os-prober
+    sed -i 's/\#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/' /mnt/etc/default/grub
 
-	# Mount boot/EFI partition
-	arch-chroot /mnt mount --mkdir /dev/sda1 /boot/EFI
+    # Mount boot/EFI partition
+    arch-chroot /mnt mount --mkdir /dev/sda1 /boot/EFI
 
-	# Print partition table
-	arch-chroot /mnt lsblk -f
+    # Print partition table
+    arch-chroot /mnt lsblk -f
 
-	# Install grub with uefi
-	arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+    # Install grub with uefi
+    arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 systems(){
-	# Enable internet, VM, Printing, Bluetooth
-	for system in NetworkManager libvirtd cups bluetooth;
-	do 
-		arch-chroot /mnt systemctl enable $system
-	done
+    # Enable internet, VM, Printing, Bluetooth
+    for system in NetworkManager libvirtd cups bluetooth;
+    do
+        arch-chroot /mnt systemctl enable $system
+    done
 }
 
 create_user(){
-	for user in work fun;
-	do 
-		arch-chroot /mnt useradd -m -G wheel,audio,video,optical,storage,libvirt -s /bin/fish $user
-		echo $user:1234 >> passwords.txt
-	done
+    for user in work fun;
+    do
+        arch-chroot /mnt useradd -m -G wheel,audio,video,optical,storage,libvirt -s /bin/fish $user
+        echo $user:1234 >> passwords.txt
+    done
 }
 
 aur(){
-	# Install Paru
-	arch-chroot /mnt sh -c '
-	su work;
-	cd /home/work/;
-	rustup default stable;
-	git clone https://aur.archlinux.org/paru-bin.git;
-	cd paru-bin;
-	makepkg -si;
-	rm paru-bin -rf;
-	'
+    # Install Paru
+    arch-chroot /mnt -u work sh -c '
+    cd /home/work;
+    rustup default stable;
+    git clone https://aur.archlinux.org/paru-bin.git;
+    cd paru-bin;
+    makepkg -si;
+    cd ..;
+    rm paru-bin -rf;
+    '
 
-	# Paru config
-	sed -i 's/\#BottomUp/BottomUp/' /mnt/etc/paru.conf
-	sed -i 's/\#RemoveMake/RemoveMake/' /mnt/etc/paru.conf
-	sed -i 's/\#CleanAfter/CleanAfter/' /mnt/etc/paru.conf
-	sed -i 's/\#\[bin\]/\[bin\]/' /mnt/etc/paru.conf
-	sed -i 's/\#FileManager = vifm/FileManager = lf/' /mnt/etc/paru.conf
-	sed -i 's/\#Sudo = doas/Sudo = \/bin\/doas/' /mnt/etc/paru.conf
-	
-	# Install aur packages
-	cp -v aur_packages.txt /mnt/home/work/
-	arch-chroot /mnt sh -c '
-	su work;
-	cd /home/work/;
-	paru --noconfirm --needed -S - < aur_packages.txt;
-	'
+    # Paru config
+    sed -i 's/\#BottomUp/BottomUp/' /mnt/etc/paru.conf
+    sed -i 's/\#RemoveMake/RemoveMake/' /mnt/etc/paru.conf
+    sed -i 's/\#CleanAfter/CleanAfter/' /mnt/etc/paru.conf
+    sed -i 's/\#\[bin\]/\[bin\]/' /mnt/etc/paru.conf
+    sed -i 's/\#FileManager = vifm/FileManager = lf/' /mnt/etc/paru.conf
+    sed -i 's/\#Sudo = doas/Sudo = \/bin\/doas/' /mnt/etc/paru.conf
+
+    # Install aur packages
+    cp -v aur_packages.txt /mnt/home/work/
+    arch-chroot /mnt -u work sh -c '
+    cd /home/work;
+    paru --noconfirm --needed -S - < aur_packages.txt;
+    '
 }
 
 x11_keymap(){
-	# Change keyboard to br
-	arch-chroot /mnt sh -c '
-	su work;
-	cd /home/work/;
-	localectl set-x11-keymap br;
-	'
+    # Change keyboard to br
+    arch-chroot /mnt -u work sh -c '
+    cd /home/work;
+    localectl set-x11-keymap br;
+    '
 }
 
 is_uefi
@@ -156,7 +154,7 @@ timedatectl set-ntp true
 # Install system foundation
 pacstrap -K /mnt base linux linux-firmware
 # Permanent mount partitions
-genfstab -U /mnt >> /mnt/etc/fstab 
+genfstab -U /mnt >> /mnt/etc/fstab
 time_and_locale
 # Set console keyboard to br
 echo 'KEYMAP=br-abnt2' >> /mnt/etc/vconsole.conf
