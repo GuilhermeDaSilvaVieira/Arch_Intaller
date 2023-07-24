690aa0c23582376b9f84d607baf5a5096023bcae
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-07-23 21:34:36 -0300

    use sda instead of vda

--- nix/install.sh
+++ nix/install.sh
@@ -1,19 +1,19 @@
 # (for BIOS systems only)
-# parted /dev/vda -- mklabel msdos
-# parted /dev/vda -- mkpart primary 1MB -16GB
-# parted /dev/vda -- mkpart primary linux-swap -16GB 100%
+# parted /dev/sda -- mklabel msdos
+# parted /dev/sda -- mkpart primary 1MB -16GB
+# parted /dev/sda -- mkpart primary linux-swap -16GB 100%
 
 # (for UEFI systems only)
-parted /dev/vda -- mklabel gpt
-parted /dev/vda -- mkpart primary 512MB -16GB
-parted /dev/vda -- mkpart primary linux-swap -16GB 100%
-parted /dev/vda -- mkpart ESP fat32 1MB 512MB
-parted /dev/vda -- set 3 esp on
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart primary 512MB -16GB
+parted /dev/sda -- mkpart primary linux-swap -16GB 100%
+parted /dev/sda -- mkpart ESP fat32 1MB 512MB
+parted /dev/sda -- set 3 esp on
 
-mkfs.ext4 -L nixos /dev/vda1
-mkswap -L swap /dev/vda2
-swapon /dev/vda2
-mkfs.fat -F 32 -n boot /dev/vda3        # (for UEFI systems only)
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mkfs.fat -F 32 -n boot /dev/sda3        # (for UEFI systems only)
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot                      # (for UEFI systems only)
 mount /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
